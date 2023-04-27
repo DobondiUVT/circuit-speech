cf98fc579bb559604ea811332a36ca020d62787f
DobondiUVT <laszlo.dobondi02@e-uvt.ro>
2023-04-27 23:06:01 +0300

    connection to sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  // output: "server",
+  // adapter: vercel()
 });
